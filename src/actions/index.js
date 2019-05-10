409270f18388a5f46cfd482a66fc44dfb9cca32e
Bradley Khan <github@bradleykhan.com>
2019-05-10 19:45:16 +0100

    Attempt to memoize network requests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import jsonPlaceHolder from "../api/jsonPlaceHolder";
+import { memoize } from "lodash";
 
 export const fetchPosts = () => {
     return async dispatch => {
@@ -8,10 +9,18 @@ export const fetchPosts = () => {
     };
 };
 
-export const fetchUser = id => {
-    return async dispatch => {
-        const response = await jsonPlaceHolder.get(`/users/${id}`);
+/* This utilises lodash's memoize to cache API requests, but has a problem.
+   By caching results from any unique argument we ensure we can never get
+   updated information about a user from subsequent API calls. */
 
-        dispatch({ type: "FETCH_USER", payload: response.data });
-    };
-};
+// export const fetchUser = id => {
+//     return dispatch => {
+//         _fetchUser(id, dispatch);
+//     };
+// };
+
+// const _fetchUser = memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceHolder.get(`/users/${id}`);
+
+//     dispatch({ type: "FETCH_USER", payload: response.data });
+// });
