dd53a6024933966527799af91d9e3ab2b55590a9
Bradley Khan <github@bradleykhan.com>
2019-05-13 19:05:18 +0100

    Implement better solution prevent excessive network requests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,29 @@
 import jsonPlaceHolder from "../api/jsonPlaceHolder";
-import { memoize } from "lodash";
+
+export const fetchPostsAndUsers = () => {
+    return async (dispatch, getState) => {
+        await dispatch(fetchPosts());
+
+        const postIds = getState().posts.map(posts => posts.userId);
+        const uniquePostIds = Array.from(new Set(postIds));
+
+        uniquePostIds.forEach(id => dispatch(fetchUser(id)));
+    };
+};
 
 export const fetchPosts = () => {
     return async dispatch => {
         const response = await jsonPlaceHolder.get("/posts");
 
         dispatch({ type: "FETCH_POSTS", payload: response.data });
+    };
+};
+
+export const fetchUser = id => {
+    return async dispatch => {
+        const response = await jsonPlaceHolder.get(`/users/${id}`);
+
+        dispatch({ type: "FETCH_USER", payload: response.data });
     };
 };
 
--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -3,10 +3,6 @@ import { connect } from "react-redux";
 import { fetchUser } from "../actions";
 
 class UserHeader extends React.Component {
-    componentDidMount() {
-        this.props.fetchUser(this.props.userId);
-    }
-
     render() {
         const { user } = this.props;
 
